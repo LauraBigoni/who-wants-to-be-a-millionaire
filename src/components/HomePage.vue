bed0bae91b8c0be05de6a4f30ebc8975814b1f1b
Laura Bigoni <laurabigoni.92@gmail.com>
2022-06-01 12:32:48 +0200

    added transition

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -19,8 +19,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-// * importo le variabili
-@import "../assets/scss/style.scss";
 button {
   width: 200px;
   margin: 20px;
